5aefa11ba4cb7c9da943fe9fc0a58e477c0386d2
robinhaider <robinhaider69@gmail.com>
2020-07-20 21:42:22 +0600

    Building an E-commerce app with .NET Core and Angular - Day twentyFour - 36 - 21. Taking Payments - paymentIntent

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -8,5 +8,8 @@ namespace API.Dtos
         [Required]
         public string Id { get; set; }
         public List<BasketItemDto> Items { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
     }
 }
