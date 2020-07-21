91963076fa4377899ff3c3418c3947d34d195547
robinhaider <robinhaider69@gmail.com>
2020-07-21 22:40:18 +0600

    Building an E-commerce app with .NET Core and Angular - Day twentyFive - 38 - 21. Taking Payments - 11

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -25,7 +25,6 @@ export class BasketService {
       .pipe(
         map((basket: IBasket) => {
           this.basketSource.next(basket);
-          console.log(this.getCurrentBasketValue());
         })
       );
   }
